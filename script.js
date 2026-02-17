59bb9ac1f3adac7338a0e5279b7378bfe4e33028
seema-124 <seemamauryadhar@gmail.com>
2026-02-17 15:58:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// var h1= document.createElement('h1')
+// h1.innerHTML="hello"
+// // console.log(h1)
+
+// var root= document.querySelector("#root")
+
+// root.appendChild(h1)
+console.log("ShopEasy website loaded!");
